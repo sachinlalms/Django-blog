f6b55bd06fd375991dfc198a49fcd8d5234624ee
SACHIN LAL MS <64086731+sachinlalms@users.noreply.github.com>
2020-07-29 13:10:21 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'postgresql-dimensional-27423',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
